096daddafadad5d4da481a47b7fc6f7d4045e049
Joao Botelho <jartsb@hotmail.com>
2025-01-10 20:01:18 +0000

    file processor testing

---
+++ spec/lib/file_processor/file_processor_spec.rb
@@ -0,0 +1,55 @@
+require 'file_processor/file_processor'
+
+# frozen_string_literal: true
+
+describe FileProcessor do
+  let(:file_content) { "Line 1\nLine 2\nLine 3\n" }
+  let(:file_path) do
+    # Create a temporary file with test content
+    tmp_file = Tempfile.new('test_file')
+    tmp_file.write(file_content)
+    tmp_file.rewind
+    tmp_file.path
+  end
+  let(:file_processor) { described_class.new(file_path) }
+
+  let(:empty_file_path) do
+    empty_tmp_file = Tempfile.new('empty_file')
+    empty_tmp_file.write("")
+    empty_tmp_file.rewind
+    empty_tmp_file.path
+  end
+  let(:empty_file_processor) {described_class.new(empty_file_path)}
+
+  describe 'preprocess' do
+    it 'calculates the correct offsets for each line' do\
+      file_processor.preprocess
+      expect(file_processor.offsets).to eq([7, 14, 21])
+    end
+
+    it 'handles successfully an empty file' do
+      empty_file_processor.preprocess
+      expect(file_processor.offsets).to eq([])
+    end
+  end
+
+  describe '#get_line' do
+    before { file_processor.preprocess }
+
+    it 'returns the correct line content for a valid line number' do
+      expect(file_processor.get_line(1)).to eq("Line 1\n")
+      expect(file_processor.get_line(2)).to eq("Line 2\n")
+      expect(file_processor.get_line(3)).to eq("Line 3\n")
+    end
+
+    it 'returns nil for an invalid line number' do
+      expect(file_processor.get_line(0)).to be_nil
+      expect(file_processor.get_line(4)).to be_nil
+    end  
+
+    it 'handles successfully an empty file' do
+      empty_file_processor.preprocess
+      expect(empty_file_processor.get_line(1)).to be_nil
+    end
+  end
+end
