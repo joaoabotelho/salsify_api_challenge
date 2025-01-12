e310842b78d6b1346dfdb60b0e6d5f1d45ba2dab
Joao Botelho <jartsb@hotmail.com>
2025-01-12 15:50:28 +0000

    remove offset array and use only cache

--- lib/file_processor/file_processor.rb
+++ lib/file_processor/file_processor.rb
@@ -3,24 +3,26 @@ class FileProcessor
 
   def initialize(file_path)
     @file_path = file_path
-    @cache_key = "file_offsets"
+    @line_count = 0
   end
 
-  # Lazily load offsets
-  def offsets
-    @offsets ||= fetch_offsets
-  end
+  # Process the file to calculate line offsets
+  def preprocess
+    offset = 0
+    @line_count = 0
 
-  # Fetch offsets from cache or preprocess the file in the cache
-  def fetch_offsets
-    Rails.cache.fetch(@cache_key) do
-      preprocess
+    File.open(file_path, "r") do |file|
+      file.each_line.with_index(1) do |line, line_number|
+        Rails.cache.write("file_offset_#{line_number}", offset)
+        offset += line.bytesize
+        @line_count = line_number
+      end
     end
   end
 
   # Get a specific line from cache or the file by its number
   def get_line(line_number)
-    return nil if line_number <= 0 || line_number > offsets.size
+    return nil if line_number <= 0 || line_number > @line_count
 
     Rails.cache.fetch("file_line_#{line_number}", expires_in: 5.minutes) do
       get_line_from_file(line_number)
@@ -31,24 +33,13 @@ class FileProcessor
 
   # Retrieve a specific line from the file by its number
   def get_line_from_file(line_number)
+    offset = Rails.cache.read("file_offset_#{line_number}")
+
     File.open(file_path, "r") do |file|
-      file.seek(offsets[line_number - 1])
+      file.seek(offset)
       file.readline
     end
   rescue EOFError
     nil
-  end
-
-  # Process the file to calculate line offsets
-  def preprocess
-    line_offsets = []
-    File.open(file_path, "r") do |file|
-      offset = 0
-      file.each_line do |line|
-        line_offsets << offset
-        offset += line.bytesize
-      end
-    end
-    line_offsets
   end
 end
