c49ee6efee4ec29e4e74dd24868603b7a1d84200
Joao Botelho <jartsb@hotmail.com>
2025-01-12 15:53:46 +0000

    update use of FileProcessor

--- config/initializers/file_service.rb
+++ config/initializers/file_service.rb
@@ -1,4 +1,4 @@
 require "file_processor/file_processor"
 
 FILE_PROCESSOR = FileProcessor.new(Rails.application.config.file_path)
-FILE_PROCESSOR.fetch_offsets
+FILE_PROCESSOR.preprocess
--- spec/lib/file_processor/file_processor_spec.rb
+++ spec/lib/file_processor/file_processor_spec.rb
@@ -11,18 +11,21 @@ describe FileProcessor do
 
   describe '#preprocess' do
     it 'calculates the correct offsets for each line' do\
-      file_processor.fetch_offsets
-      expect(file_processor.offsets).to eq([ 0, 7, 14 ])
+      file_processor.preprocess
+      expect(Rails.cache.read("file_offset_1")).to eq(0)
+      expect(Rails.cache.read("file_offset_2")).to eq(7)
+      expect(Rails.cache.read("file_offset_3")).to eq(14)
+      expect(Rails.cache.read("file_offset_4")).to be_nil
     end
 
     it 'handles successfully an empty file' do
-      empty_file_processor.fetch_offsets
-      expect(empty_file_processor.offsets).to eq([])
+      empty_file_processor.preprocess
+      expect(Rails.cache.read("file_offset_1")).to be_nil
     end
   end
 
   describe '#get_line' do
-    before { file_processor.fetch_offsets }
+    before { file_processor.preprocess }
 
     it 'returns the correct line content for a valid line number' do
       expect(file_processor.get_line(1)).to eq("Line 1\n")
@@ -36,15 +39,8 @@ describe FileProcessor do
     end
 
     it 'handles successfully an empty file' do
-      empty_file_processor.fetch_offsets
+      empty_file_processor.preprocess
       expect(empty_file_processor.get_line(1)).to be_nil
-    end
-  end
-
-  describe 'Caching' do
-    it 'stores offsets in cache after preprocessing' do
-      file_processor.fetch_offsets
-      expect(Rails.cache.read("file_offsets")).to eq([ 0, 7, 14 ])
     end
   end
 end
